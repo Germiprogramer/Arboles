4b96c4c7e6577122c516dab4bb5e67071cd08136
Germiprogramer <llorentemunozgerman@gmail.com>
2022-11-05 11:29:02 +0100

    commit

--- arbol.py
+++ arbol.py
@@ -18,9 +18,22 @@ def insertarnodo(raiz, dato):
                 raiz.izquierda = nodoArbol(dato)
             elif raiz.info > dato and raiz.izquierda != None:
                 raiz = raiz.izquierda
-        
 
-insertarnodo(None, nodoArbol(2))
+def inorden(raiz):     
+    if raiz.info is not None:
+        inorden(raiz.izquierda)  
+        print(raiz.info)
+        inorden(raiz.derecha)
+    else:
+        print("fallo")
 
+raiz = nodoArbol(2)
+insertarnodo(raiz, 4)
+insertarnodo(raiz,5)
+insertarnodo(raiz, 1)
+
+inorden(raiz)
+
+print("hola")
 
                 
