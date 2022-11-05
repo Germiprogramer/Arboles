1dceb3a634aeb9c7ae09717e0da38990d1204769
Germiprogramer <llorentemunozgerman@gmail.com>
2022-11-05 11:45:20 +0100

    commit

--- arbol.py
+++ arbol.py
@@ -7,20 +7,24 @@ class nodoArbol():
 def insertarnodo(raiz, dato):
     if raiz == None:
         print("el nodo es la raiz")
-        raiz = dato
+        raiz = nodoArbol(dato)
     else:
-        while True:
+        while raiz.info!= dato:
             if raiz.info <= dato and raiz.derecha == None:
                 raiz.derecha = nodoArbol(dato)
+                print("a")
             elif raiz.info <= dato and raiz.derecha != None:
                 raiz = raiz.derecha
+                print("b")
             elif raiz.info > dato and raiz.izquierda == None:
                 raiz.izquierda = nodoArbol(dato)
+                print("c")
             elif raiz.info > dato and raiz.izquierda != None:
                 raiz = raiz.izquierda
+                print("d")
 
 def inorden(raiz):     
-    if raiz.info is not None:
+    if raiz is not None:
         inorden(raiz.izquierda)  
         print(raiz.info)
         inorden(raiz.derecha)
@@ -30,10 +34,12 @@ def inorden(raiz):
 raiz = nodoArbol(2)
 insertarnodo(raiz, 4)
 insertarnodo(raiz,5)
-insertarnodo(raiz, 1)
+insertarnodo(raiz,1)
+insertarnodo(raiz,0)
 
 inorden(raiz)
 
-print("hola")
+
+
 
                 
